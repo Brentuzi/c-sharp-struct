fb8969b745dd540ebc30d9c99e9d3585b5c7d52d
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-02-01 21:46:49 +0300

    Add files via upload

---
+++ ITMO.Lab 1.3/ITMO.Lab 1.3/nForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.Lab_1._3
+{
+    public partial class nForm : ITMO.Lab_1._3.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
