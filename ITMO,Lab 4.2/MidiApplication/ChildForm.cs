c58c690cb1dce5fa3cf85096a12cc7a691ef5951
Brentuzi <90348225+Brentuzi@users.noreply.github.com>
2022-02-02 12:40:11 +0300

    Add files via upload

---
+++ ITMO,Lab 4.2/MidiApplication/ChildForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MidiApplication
+{
+    public partial class ChildForm : MidiApplication.ParentForm
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChildForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
